45d78c5b8de01d65557ea114c8adb378f1cbdc06
hoshi <hoshi@it.nkk.ac.jp>
2023-01-16 16:50:04 +0900

    Initial commit

---
+++ DataType.cs
@@ -0,0 +1,135 @@
+﻿// <DataType.cs> 2023/01/16完了分
+
+namespace AccountBook;  // ファイル全体の名前空間
+
+// 日付を扱うクラス
+public class Date
+{
+    // 内部で保持する日時値（時刻は常に0:00:00とする）
+    public DateTime Value { get; private set; } = default;
+
+    // 値の文字列表現（オーバーライド）
+    public override string ToString()
+    {
+        // 内部の値を短い日付表記で返す
+        return Value.ToShortDateString();
+    }
+
+    // デフォルトコンストラクター
+    public Date()
+    {
+        // 何もしない（既定値が設定される）
+    }
+
+    // 日時を受け取るコンストラクター
+    public Date(DateTime dt)
+    {
+        // 日付部分のみ取り出して設定する
+        Value = dt.Date;
+    }
+
+    // 文字列を受け取るコンストラクタ―
+    public Date(string str)
+    {
+        // 文字列から日時型に変換する（失敗したら例外）
+        var dt = DateTime.Parse(str);
+        // 日付部分のみ取り出して設定する
+        Value = dt.Date;
+    }
+
+    // Date型からDateTime型への暗黙的キャスト演算子のオーバーロード
+    public static implicit operator DateTime(Date d)
+    {
+        // 内部で保持する日時を返す
+        return d.Value;
+    }
+
+    // DateTime型からDate型への明示的キャスト演算子のオーバーロード
+    public static explicit operator Date(DateTime dt)
+    {
+        // 日時から新規日付を生成して返す
+        return new Date(dt);
+    }
+
+    // Date型からstring型への暗黙的キャスト演算子のオーバーロード
+    public static implicit operator string(Date d)
+    {
+        // 日付の文字列表現を返す
+        return d.ToString();
+    }
+
+    // string型からDate型への暗黙的キャスト演算子のオーバーロード
+    public static implicit operator Date(string str)
+    {
+        // 文字列から新規日付を生成して返す
+        return new Date(str);
+    }
+}
+
+
+// 金銭を扱う構造体
+public struct Money
+{
+    // 内部で保持する十進型の数値（正の整数のみとする）
+    public decimal Value { get; private set; } = default;
+
+    // 値の文字列表現（オーバーライド）
+    public override string ToString()
+    {
+        // 内部の値を通貨形式で返す
+        return Value.ToString("c");
+    }
+
+    // デフォルトコンストラクター
+    public Money()
+    {
+        // 何もしない（既定値が設定される）
+    }
+
+    // decimal型を受け取るコンストラクタ―
+    public Money(decimal dc)
+    {
+        // 負の値の場合、0を設定して終わる
+        if (dc < 0)
+        {
+            Value = 0;
+            return;
+        }
+        // 精密な四捨五入をした整数を設定する
+        Value = Math.Round(dc, MidpointRounding.AwayFromZero);
+    }
+
+    // string型を受け取るコンストラクタ―
+    public Money(string str) : this(decimal.Parse(str))
+    {
+        // 文字列を十進型に変換したものを別のコンストラクタ―に渡す
+    }
+
+    // decimal型からMoney型への暗黙的キャスト演算子のオーバーロード
+    public static implicit operator Money(decimal dc)
+    {
+        // 数値から新たな金銭を生成して返す
+        return new Money(dc);
+    }
+
+    // Money型からdecimal型への暗黙的キャスト演算子のオーバーロード
+    public static implicit operator decimal(Money m)
+    {
+        // 内部で保持する値を返す
+        return m.Value;
+    }
+
+    // Money型からint型への暗黙的キャスト演算子のオーバーロード
+    public static implicit operator int(Money m)
+    {
+        // 内部で保持する値を明示的にint型にキャストして返す
+        return (int)m.Value;
+    }
+
+    // Money型からstring型への明示的キャスト演算子のオーバーロード
+    public static explicit operator string(Money m)
+    {
+        // 内部で保持する値を通常の文字列変換を行って返す
+        return m.Value.ToString();
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+namespace AccountBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var d1 = new Date();                    // 引数なし
+            var d2 = new Date(DateTime.Now);        // DateTime型（Nowは今現在の日時）
+            var d3 = new Date("2023/1/23");     // 文字列
+            Debug.WriteLine(d1.Value);                      // Valueプロパティを表示
+            Debug.WriteLine(d2.Value.ToString());               // Valueの文字列表現を表示
+            Debug.WriteLine(d3.Value.ToShortDateString());  // Valueの短い日付表現を表示
+            Debug.WriteLine(d2.Value.ToLongDateString());       // Valueの長い日付表現を表示
+            Debug.WriteLine(d3);                                // インスタンスをそのまま表示
+
+            var d4 = (Date)DateTime.Now;    // DateTimeをDateにキャストして代入(d1はDate型になる)
+            Date d5 = "2023/1/23";		// d5がDate型なので暗黙的にDateにキャストして代入 
+
+        }
+    }
+}
