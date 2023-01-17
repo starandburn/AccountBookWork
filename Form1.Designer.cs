ec4ffa75c7b5f81f99d9b10e9f37de9797d9a430
hoshi <hoshi@it.nkk.ac.jp>
2023-01-17 15:15:11 +0900

    2023/1/17授業で作成

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -135,7 +135,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(303, 172);
+            this.ClientSize = new System.Drawing.Size(303, 167);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.txtRemarks);
             this.Controls.Add(this.label5);
