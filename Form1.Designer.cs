9a188ede2fbfc5a5d102dee07bdd4bf5d9080a9a
hoshi <hoshi@it.nkk.ac.jp>
2023-01-17 09:05:55 +0900

    フォントサイズ変更

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,88 +44,99 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 26);
+            this.label1.Location = new System.Drawing.Point(27, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.Size = new System.Drawing.Size(42, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "日付";
             // 
             // txtDate
             // 
-            this.txtDate.Location = new System.Drawing.Point(65, 22);
+            this.txtDate.Location = new System.Drawing.Point(84, 31);
+            this.txtDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtDate.Name = "txtDate";
-            this.txtDate.Size = new System.Drawing.Size(100, 23);
+            this.txtDate.Size = new System.Drawing.Size(127, 29);
             this.txtDate.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 55);
+            this.label2.Location = new System.Drawing.Point(27, 77);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.Size = new System.Drawing.Size(42, 21);
             this.label2.TabIndex = 0;
             this.label2.Text = "分類";
             // 
             // txtCategory
             // 
-            this.txtCategory.Location = new System.Drawing.Point(65, 51);
+            this.txtCategory.Location = new System.Drawing.Point(84, 71);
+            this.txtCategory.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCategory.Name = "txtCategory";
-            this.txtCategory.Size = new System.Drawing.Size(100, 23);
+            this.txtCategory.Size = new System.Drawing.Size(127, 29);
             this.txtCategory.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 84);
+            this.label3.Location = new System.Drawing.Point(27, 118);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.Size = new System.Drawing.Size(42, 21);
             this.label3.TabIndex = 0;
             this.label3.Text = "品名";
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(65, 80);
+            this.txtName.Location = new System.Drawing.Point(84, 112);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(100, 23);
+            this.txtName.Size = new System.Drawing.Size(127, 29);
             this.txtName.TabIndex = 1;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(21, 113);
+            this.label4.Location = new System.Drawing.Point(27, 158);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 15);
+            this.label4.Size = new System.Drawing.Size(42, 21);
             this.label4.TabIndex = 0;
             this.label4.Text = "金額";
             // 
             // txtAmount
             // 
-            this.txtAmount.Location = new System.Drawing.Point(65, 109);
+            this.txtAmount.Location = new System.Drawing.Point(84, 153);
+            this.txtAmount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtAmount.Name = "txtAmount";
-            this.txtAmount.Size = new System.Drawing.Size(100, 23);
+            this.txtAmount.Size = new System.Drawing.Size(127, 29);
             this.txtAmount.TabIndex = 1;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(21, 142);
+            this.label5.Location = new System.Drawing.Point(27, 199);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(31, 15);
+            this.label5.Size = new System.Drawing.Size(42, 21);
             this.label5.TabIndex = 0;
             this.label5.Text = "備考";
             // 
             // txtRemarks
             // 
-            this.txtRemarks.Location = new System.Drawing.Point(65, 138);
+            this.txtRemarks.Location = new System.Drawing.Point(84, 193);
+            this.txtRemarks.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtRemarks.Name = "txtRemarks";
-            this.txtRemarks.Size = new System.Drawing.Size(100, 23);
+            this.txtRemarks.Size = new System.Drawing.Size(127, 29);
             this.txtRemarks.TabIndex = 1;
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(211, 24);
+            this.btnOK.Location = new System.Drawing.Point(271, 34);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.Size = new System.Drawing.Size(96, 32);
             this.btnOK.TabIndex = 2;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -133,9 +144,9 @@
             // 
             // EntryForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(307, 192);
+            this.ClientSize = new System.Drawing.Size(395, 269);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.txtRemarks);
             this.Controls.Add(this.label5);
@@ -147,7 +158,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtDate);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "EntryForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
